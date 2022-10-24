e05ed0fa3db7ac578afb7d951cb4f4bc0c97bacc
Anton Yefanov <antonyefanov@gmail.com>
2022-10-24 10:47:53 +0300

    minor changes

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,7 +1,6 @@
 import { Favorites } from "../../components/Favorites";
 import { Header } from "../../components/Header";
 import { Quotes } from "../../components/Quotes";
-import { Settings } from "../../components/Settings";
 
 const Main = () => {
   return (
